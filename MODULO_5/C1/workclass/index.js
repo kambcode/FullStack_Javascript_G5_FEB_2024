41949e00eb9b95dbec23250840d7d6131edfdb67
Jose Trespalacios <josetrespalaciosbedoya@gmail.com>
2024-06-17 20:00:47 -0500

    Condicionales C1

--- MODULO_5/C1/workclass/index.js
+++ MODULO_5/C1/workclass/index.js
@@ -1,7 +1,14 @@
 let edad = 25;
+let precio = 1500;
 
 if (edad>18) {
   console.log('Es mayor de edad');
 } else {
   console.log('No es mayor de edad')
+}
+
+if(precio>=2000) {
+  console.log('Este producto tiene impuesto');
+} else {
+  console.log('Este producto no tiene impuesto');
 }
