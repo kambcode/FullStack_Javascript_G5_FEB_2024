d2b84fb3efaef64bd90006c54cf4600ac6ec47f2
Jose Trespalacios <josetrespalaciosbedoya@gmail.com>
2024-06-17 20:23:53 -0500

    Funciones

--- MODULO_5/C1/workclass/index.js
+++ MODULO_5/C1/workclass/index.js
@@ -1,6 +1,6 @@
 let edad = 25;
 let precio = 1500;
-
+/** Condicionales basicas **/
 if (edad>18) {
   console.log('Es mayor de edad');
 } else {
@@ -11,4 +11,17 @@ if(precio>=2000) {
   console.log('Este producto tiene impuesto');
 } else {
   console.log('Este producto no tiene impuesto');
+}
+
+/** Funciones basicas **/
+function addNumbers(a, b){
+  /**Acá, va la lógica de nuestra función.**/
+  console.log(a+b);
+}
+
+/**Esta es la manera de invocar una función**/
+addNumbers(1,5)
+
+function nombreDeLaFuncion(parametro1, parametro2) {
+  // Cuerpo de la función
 }
