af2c3df828801d0f148bb398f9251073db379d00
Jose Trespalacios <josetrespalaciosbedoya@gmail.com>
2024-06-19 20:56:36 -0500

    Base project node.js

---
+++ MODULO_5/C2/workclass/project-jose/main.js
@@ -0,0 +1,11 @@
+function obtenerEmpresa() {
+    return {
+        nombre: "Mi Empresa",
+        fundacion: 2000,
+        sede: "Ciudad de Ejemplo",
+        industria: "Tecnología",
+        empleados: 100
+    };
+}
+
+module.exports = obtenerEmpresa;
