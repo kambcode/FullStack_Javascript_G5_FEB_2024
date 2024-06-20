8fc69b3804359ecc657d4d7edeffd9458ad95c43
Jose Trespalacios <josetrespalaciosbedoya@gmail.com>
2024-06-20 10:28:24 -0500

    C2 Grabacion

--- MODULO_5/C2/workclass/project-jose/index.js
+++
@@ -1,9 +0,0 @@
-const obtenerEmpresa = require('./main');
-
-const empresa = obtenerEmpresa();
-console.log("Información de la empresa:");
-console.log("Nombre:", empresa.nombre);
-console.log("Fundación:", empresa.fundacion);
-console.log("Sede:", empresa.sede);
-console.log("Industria:", empresa.industria);
-console.log("Número de empleados:", empresa.empleados);
