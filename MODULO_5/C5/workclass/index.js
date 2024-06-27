26a8fa482ee75e5456ff8058ac9c547b1eb9fb8a
Jose Trespalacios <josetrespalaciosbedoya@gmail.com>
2024-06-26 20:57:25 -0500

    Base FS Read and create file

--- MODULO_5/C5/workclass/index.js
+++ MODULO_5/C5/workclass/index.js
@@ -1,9 +1,47 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 const port = 8000;
 
 app.get('/', (req, res) => {
-    res.send('Welcome');
+    fs.writeFile('jose.txt', 'Hello, Jose!', (err)=> {
+        if(err) {
+            res.send('Error al crear archivo');
+            return console.log(err);
+        }
+        res.send('Archivo creado exitosamente');
+    });
+});
+
+app.get('/create-rar', (req, res) => {
+    fs.writeFile('rar.txt', 'Hello, RAR!', (err)=> {
+        if(err) {
+            res.send('Error al crear archivo');
+            return console.log(err);
+        }
+        res.send('Archivo creado exitosamente');
+    });
+});
+
+app.get('/read', (req, res) => {
+    fs.readFile('jose.txt', 'utf8', (err, data)=> {
+        if(err) {
+            res.send('Error al leer archivo');
+            return console.log(err);
+        }
+        res.send(`Contenido del archivo: ${data}`);
+    });
+});
+
+app.get('/read-rar', (req, res) => {
+    fs.readFile('rar.txt', 'utf8', (err, data)=> {
+        if(err) {
+            res.send('Error al leer archivo');
+            return console.log(err);
+        }
+        res.send(`Contenido del archivo: ${data}`);
+    });
 });
 
 app.listen(port, () => {
