ab87456153af3a786538928a5f4ec8521e5ba33f
Jose Trespalacios <josetrespalaciosbedoya@gmail.com>
2024-07-26 20:53:24 -0500

    Query operations basic C3-M6 Bases de datos

---
+++ MODULO_6/BaseDeDatos-C3/workclass/create_database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE tienda_harold
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Latin America.1252'
+    LC_CTYPE = 'Spanish_Latin America.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
